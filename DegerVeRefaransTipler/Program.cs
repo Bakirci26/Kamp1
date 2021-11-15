a71b44e4cdfb93b1a03309145a99e29f9348f409
MUSTAFA BAKIRCI <2013010217086@ogrenci.karabuk.edu.tr>
2021-11-16 00:58:26 +0300

    16.11.21

---
+++ DegerVeRefaransTipler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DegerVeRefaransTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            Console.WriteLine(sayi1);//30       int,decimal,float,double,bool=değertip(stack)
+
+            int[] sayılar1 = new int[] { 10, 20, 30 };
+            int[] sayılar2 = new int[] { 100, 200, 300 };
+            sayılar1 = sayılar2;
+            sayılar2[0] = 999;
+            Console.WriteLine(sayılar1[0]);//999      array,class,interface = referanstip 
+
+
+
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(2, 9);
+            dortIslem.Topla(4, 8);
+            dortIslem.Topla(5, 3);
+        }
+    }
+}
